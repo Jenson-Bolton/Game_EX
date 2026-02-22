f69af8d5cfed56ce9454c28b7b622e7556952148
jenson-bolton-ac <69874214+Jenson-Bolton@users.noreply.github.com>
2026-02-22 12:18:00 +0000

    submodule update
    temp
    
    logger
    
    .gitignore updated
    
    Squshing commits:
    - 81ed031 logger
    - afae06b .gitignore updated
    
    change cmake preset to ninja for win-debug
    
    Squahing commits:
    - 81ed031 logger
    - afae06b .gitignore updated
    - fe051e9 change cmake preset to ninja for win-debug

--- src/engine/core/log/Log.cpp
+++ src/engine/core/log/Log.cpp
@@ -1,9 +1,37 @@
 #include "Log.h"
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
-#include <SDL3/SDL.h>
 
 std::shared_ptr<spdlog::logger> Log::s_Logger;
+
+/**
+ * @brief SDL log output callback.
+ *
+ * Redirects SDL logging into the engine logging system.
+ *
+ * @param userdata User data pointer (unused).
+ * @param category SDL log category.
+ * @param priority SDL log priority.
+ * @param message Log message string.
+ */
+static void SDLCALL SDLLogCallback(void* userdata, int category, SDL_LogPriority priority, const char* message)
+{
+	switch (priority)
+	{
+	case SDL_LOG_PRIORITY_ERROR:
+		LOG_ERROR("[SDL] {}", message);
+		break;
+	case SDL_LOG_PRIORITY_WARN:
+		LOG_WARN("[SDL] {}", message);
+		break;
+	case SDL_LOG_PRIORITY_INFO:
+		LOG_INFO("[SDL] {}", message);
+		break;
+	default:
+		LOG_DEBUG("[SDL] {}", message);
+		break;
+	}
+}
 
 void Log::Init()
 {
@@ -15,7 +43,7 @@ void Log::Init()
 
 	std::vector<spdlog::sink_ptr> sinks{ console_sink, file_sink };
 
-	s_Logger = std::make_shared<spdlog::logger>("GAME", sink.begin(), sinks.end());
+	s_Logger = std::make_shared<spdlog::logger>("GAME", sinks.begin(), sinks.end());
 	spdlog::register_logger(s_Logger);
 
 #ifdef NDEBUG
@@ -23,25 +51,7 @@ void Log::Init()
 #else
 	s_Logger->set_level(spdlog::level::trace);
 #endif
-}
 
-void SDLLogCallback(void* userdata, int category, SDL_LogPriority priority, const char* message)
-{
-	switch (priority)
-	{
-		case SDL_LOG_PRIORITY_ERROR:
-			LOG_ERROR("[SDL] {}", message);
-			break;
-		case SDL_LOG_PRIORITY_WARN:
-			LOG_WARN("[SDL] {}", message);
-			break;
-		case SDL_LOG_PRIORITY_INFO:
-			LOG_INFO("[SDL] {}", message);
-			break;
-		default:
-			LOG_DEBUG("[SDL] {}", message);
-			break;
-	}
+	// Redirect SDL logging into engine logger
+	SDL_SetLogOutputFunction(SDLLogCallback, nullptr);
 }
-
-SDL_LogSetOutputFunction(SDLLogCallback, nullptr);
