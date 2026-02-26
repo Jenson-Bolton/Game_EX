6fde7cbff0df2b3b4a97623ce06e47a7e11ec3ee
jenson-bolton-ac <69874214+Jenson-Bolton@users.noreply.github.com>
2026-02-26 00:17:06 +0000

    SDL3 window created

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -1,21 +1,60 @@
 #include <filesystem>
 
+#include "platform/window/sdl/SdlWindow.h"
 #include "engine/core/log/Log.h"
 
-#include "game/HelloTriangleGame.h"
 
-int main()
+int main(int /*argc*/, char** /*argv*/)
 {
 	// --- Logging ---
 	std::filesystem::create_directories("logs");
 
 	Log::Init();
-
-
 	LOG_INFO("Game starting...");
 	LOG_DEBUG("Debug logging enabled");
 
-	// sdl etc...
+	platform::window::WindowDesc wd{};
+	wd.width = 1280;
+	wd.height = 720;
+	wd.title = "Game_EX";
+	wd.resizable = true;
 
-	//HelloTriangle::run();
+	platform::window::SdlWindow window;
+	if (!window.create(wd)) {
+		LOG_CRITICAL("Failed to create window!");
+		return 1;
+	}
+
+	// auto renderer = create_renderer(); // std::unique_ptr<IRenderer>
+	// if (!renderer || !renderer->initialize(window)) return 1;
+
+	while (!window.should_close())
+	{
+		window.poll_events();
+
+		if (window.is_minimized()) {
+			// sleep, no render, etc. save power
+			continue;
+		}
+
+		if (window.was_resized()) {
+			int fb_w = 0, fb_h = 0;
+			window.get_framebuffer_size(fb_w, fb_h);
+
+			if (fb_w > 0 && fb_h > 0) {
+				// renderer->resize((uint32_t)fb_w, (uint32_t)fb_h);
+			}
+
+			window.clear_resized_flag();
+
+			// renderer->begin_frame();
+			// update_game(dt);
+			// renderer->end_frame();
+		}
+	}
+
+	// renderer->shutdown();
+	window.destroy();
+	return 0;
+
 }
