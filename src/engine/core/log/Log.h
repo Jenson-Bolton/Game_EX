0ebb8b0b5fa98f80380c16dcd59ea98b33044667
jenson-bolton-ac <69874214+Jenson-Bolton@users.noreply.github.com>
2026-02-26 10:33:18 +0000

    Refactored Log

--- src/engine/core/log/Log.h
+++ src/engine/core/log/Log.h
@@ -4,49 +4,58 @@
 #include <spdlog/spdlog.h>
 #include <SDL3/SDL.h>
 
+
 /**
- * @brief Central logging system for the engine.
- *
- * Wraps spdlog and provides:
- * - Console logging
- * - File logging
- * - SDL log redirection
- *
- * This system must be initialized once at application startup
- * before any logging macros are used.
+ * @file Log.h
+ * @brief spglog implmentation.
  */
-class Log
+
+namespace engine::core
 {
-public:
     /**
-     * @brief Initializes the logging system.
+     * @brief Central logging system for the engine.
      *
-     * Creates console and file sinks and configures
-     * log level depending on build type.
+     * Wraps spdlog and provides:
+     * - Console logging
+     * - File logging
+     * - SDL log redirection
      *
-     * Must be called once before using LOG_* macros.
+     * This system must be initialized once at application startup
+     * before any logging macros are used.
      */
-	static void Init();
+    class Log
+    {
+    public:
+        /**
+         * @brief Initializes the logging system.
+         *
+         * Creates console and file sinks and configures
+         * log level depending on build type.
+         *
+         * Must be called once before using LOG_* macros.
+         */
+	    static void init();
 
-    /**
-     * @brief Returns the underlying spdlog logger instance.
-     *
-     * @return Shared pointer to the logger.
-     */
-	inline static std::shared_ptr<spdlog::logger>& GetLogger()
-	{
-		return s_Logger;
-	}
+        /**
+         * @brief Returns the underlying spdlog logger instance.
+         *
+         * @return Shared pointer to the logger.
+         */
+	    inline static std::shared_ptr<spdlog::logger>& get_logger()
+	    {
+		    return logger_;
+	    }
 
-private:
-    /// Global logger instance
-	static std::shared_ptr<spdlog::logger> s_Logger;
-};
+    private:
+        /// Global logger instance
+	    static std::shared_ptr<spdlog::logger> logger_;
+    };
+} // namespace engine::core
 
 /// Logging macros
-#define LOG_TRACE(...)    Log::GetLogger()->trace(__VA_ARGS__)
-#define LOG_DEBUG(...)    Log::GetLogger()->debug(__VA_ARGS__)
-#define LOG_INFO(...)     Log::GetLogger()->info(__VA_ARGS__)
-#define LOG_WARN(...)     Log::GetLogger()->warn(__VA_ARGS__)
-#define LOG_ERROR(...)    Log::GetLogger()->error(__VA_ARGS__)
-#define LOG_CRITICAL(...) Log::GetLogger()->critical(__VA_ARGS__)
+#define LOG_TRACE(...)    engine::core::Log::get_logger()->trace(__VA_ARGS__)
+#define LOG_DEBUG(...)    engine::core::Log::get_logger()->debug(__VA_ARGS__)
+#define LOG_INFO(...)     engine::core::Log::get_logger()->info(__VA_ARGS__)
+#define LOG_WARN(...)     engine::core::Log::get_logger()->warn(__VA_ARGS__)
+#define LOG_ERROR(...)    engine::core::Log::get_logger()->error(__VA_ARGS__)
+#define LOG_CRITICAL(...) engine::core::Log::get_logger()->critical(__VA_ARGS__)
